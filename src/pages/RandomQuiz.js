dd1fafc603903f2a6b6dbe53b23df95b9d4b0840
mohamadiqbalamrullah <miqbalboy19@gmail.com>
2022-11-30 21:44:36 +0700

    formatted

--- src/pages/RandomQuiz.js
+++ src/pages/RandomQuiz.js
@@ -2,6 +2,21 @@ import React from "react";
 import Template from "../components/Template";
 
 export default function RandomQuiz() {
+    React.useEffect(() => {
+        (async function () {
+            await axios
+                .get(`${BASE_URL}/quiz`)
+                .then(response => {
+                    const result = response.data;
+                    setQuiz(result.data);
+                    console.log(result.data);
+                })
+                .catch(error => {
+                    console.error(error);
+                })
+        })()
+    }, []);
+    
     return (
         <Template title="Jawab Quiz Random">
             
